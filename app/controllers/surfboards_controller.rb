bc88d205aef08b11c1c18431c8c509475c47fcab
Guillaume Olivier <guillaumeolivier8@gmail.com>
2024-08-20 17:56:17 +0100

    Create a surfboard

--- app/controllers/surfboards_controller.rb
+++ app/controllers/surfboards_controller.rb
@@ -1,2 +1,23 @@
 class SurfboardsController < ApplicationController
+
+  def new
+    @surfboard = Surfboard.new
+  end
+
+  def create
+    @surfboard = Surfboard.new(list_params)
+    @surfboard.user = current_user
+    if @surfboard.save
+      redirect_to surfboards_path # change path to have surfboard id?
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:surfboard).permit(:name, :details, :price, :address, :photo)
+  end
+
 end
